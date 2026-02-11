3c494679453ca92df03f973ee096d24c79357d35
Rajeswari Ankem <rajeswariankem@gmail.com>
2026-02-11 20:41:18 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Registrationform from "./Components/Registrationform";
+function App() {
+  return (
+    <div>
+      <h1>Registration Form</h1>
+      <Registrationform />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Registrationform.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Registrationform() {
+  return <div>Registrationform</div>;
+}
+
+export default Registrationform;
